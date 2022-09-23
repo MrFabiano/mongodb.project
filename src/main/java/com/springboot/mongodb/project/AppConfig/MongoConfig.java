d0cc2df7aeadd322cadd2ce81184e04ded747cd7
fabiano <fabianosanttoss@hotmail.com>
2022-09-23 18:12:57 -0300

    Conexão mongoDB com SpringBoot

--- src/main/java/com/springboot/mongodb/project/AppConfig/MongoConfig.java
+++ src/main/java/com/springboot/mongodb/project/AppConfig/MongoConfig.java
@@ -7,7 +7,6 @@ import com.mongodb.client.MongoClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
-import org.springframework.data.mongodb.core.MongoTemplate;
 
 import java.util.Collection;
 import java.util.Collections;
