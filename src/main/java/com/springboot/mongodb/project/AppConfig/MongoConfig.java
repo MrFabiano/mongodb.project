b1b377c3e2c6ec520ac24269578ae488df676564
fabiano <fabianosanttoss@hotmail.com>
2022-10-14 00:58:17 -0300

    ajustando conflitos internos

--- src/main/java/com/springboot/mongodb/project/AppConfig/MongoConfig.java
+++ src/main/java/com/springboot/mongodb/project/AppConfig/MongoConfig.java
@@ -4,6 +4,7 @@ import com.mongodb.ConnectionString;
 import com.mongodb.MongoClientSettings;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
@@ -14,6 +15,15 @@ import java.util.Collections;
 @Configuration
 public class MongoConfig extends AbstractMongoClientConfiguration {
 
+    public static final String MONGODB_CONNECT = "mongodb+srv://%s:%s@%s";
+
+    @Value("${mongodb.username}")
+    private String username;
+    @Value("${mongodb.password}")
+    private String password;
+    @Value("${mongodb.host}")
+    private String host;
+
 
     @Override
     protected String getDatabaseName() {
@@ -23,7 +33,8 @@ public class MongoConfig extends AbstractMongoClientConfiguration {
  
     @Bean
     public MongoClient mongoClient(){
-        ConnectionString connectionString = new ConnectionString("mongodb+srv://seuenderecodebanco");
+        //ConnectionString connectionString = new ConnectionString("mongodb+srv://mongodbjavaspring05:CASA_JAVA@cluster0.fzg9tyo.mongodb.net");
+        ConnectionString connectionString = new ConnectionString(String.format(MONGODB_CONNECT, username, password, host));
         MongoClientSettings mongoClientSettings = MongoClientSettings.builder()
                 .applyConnectionString(connectionString)
                 .build();
